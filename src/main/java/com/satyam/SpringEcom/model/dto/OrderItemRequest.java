bfb7552d80b8bb43034d405af8c1013ced884393
ks9430565 <90494688+ks9430565@users.noreply.github.com>
2025-10-03 12:22:49 +0530

    Creating DTO and models for Order part of project

---
+++ src/main/java/com/satyam/SpringEcom/model/dto/OrderItemRequest.java
@@ -0,0 +1,5 @@
+package com.satyam.SpringEcom.model.dto;
+
+public record OrderItemRequest(int productId,int quantity) {
+
+}
---
+++ src/main/java/com/satyam/SpringEcom/model/dto/OrderItemResponse.java
@@ -0,0 +1,7 @@
+package com.satyam.SpringEcom.model.dto;
+
+import java.math.BigDecimal;
+
+public record OrderItemResponse(String productName, int quantity, BigDecimal totalPrice) {
+
+}
